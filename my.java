001b34b168387322faa5b6e0e3afa322ef0f0a90
Nagasha-Trecy <nagashatrecy@gmail.com>
2019-11-22 19:47:55 +0300

    portfolio

---
+++ my.java
@@ -0,0 +1,10 @@
+class Main{
+	public static void public static void main(String[] args) {
+	
+	System.out.println("Hello"+ "World");
+
+	System.out.println("38"+"19");
+
+	System.out.println("38+19")	
+	}
+}
